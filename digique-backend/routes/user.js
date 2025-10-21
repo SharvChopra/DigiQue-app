a775079c00cf3ff4c19c2b8e6441870cef5f7cc1
Sharv Chopra <sharvchopra12@gmail.com>
2025-10-21 19:43:56 +0530

    Updated the user routing to add settings routes

--- digique-backend/routes/user.js
+++ digique-backend/routes/user.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const authMiddleware = require("../middleware/auth.js"); 
+const authMiddleware = require("../middleware/auth.js");
 const User = require("../models/User.js");
+const UpdateHistory = require("../models/History.js");
 const router = express.Router();
 
-router.get("/role", authMiddleware, async (req, res) => {
+const formatFieldName = (key) => {
+  return key.replace(/([A-Z])/g, " $1");
+};
+
+router.get("/me", authMiddleware, async (req, res) => {
   try {
     const user = await User.findById(req.user.id).select("-password");
     if (!user) {
@@ -12,6 +17,95 @@ router.get("/role", authMiddleware, async (req, res) => {
     res.status(200).json({ role: user.role, user });
   } catch (error) {
     res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+router.put("/me", authMiddleware, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id);
+    if (!user)
+      return res.status(404).json({
+        msg: "User not Found",
+      });
+    const updates = req.body;
+    const historyLogs = [];
+    Object.keys(updates).forEach((key) => {
+      const newValue = updates[key];
+      const previousValue = user[key];
+
+      if (
+        typeof newValue === "object" &&
+        newValue !== null &&
+        !Array.isArray(newValue)
+      ) {
+        Object.keys(newValue).forEach((nestedKey) => {
+          const newNestedValue = newValue[nestedKey];
+          const oldNestedValue = previousValue
+            ? previousValue[nestedKey]
+            : undefined;
+
+          if (
+            oldNestedValue !== undefined &&
+            String(oldNestedValue) !== String(newNestedValue)
+          ) {
+            historyLogs.push({
+              user: user._id,
+              field: `${formatFieldName(key)} - ${formatFieldName(nestedKey)}`,
+              previousValue: String(oldNestedValue),
+              newValue: String(newNestedValue),
+            });
+          }
+        });
+      } else if (
+        previousValue != undefined &&
+        String(previousValue) !== String(newValue)
+      ) {
+        if (key == "dateOfBirth") {
+          const oldDate = new Date(previousValue).toLocaleDateString();
+          const newDate = new Date(newValue).toLocaleDateString();
+          if (oldDate !== newDate) {
+            historyLogs.push({
+              user: user._id,
+              field: formatFieldName(key),
+              previousValue: oldDate,
+              newValue: newDate,
+            });
+          }
+        } else {
+          historyLogs.push({
+            user: user._id,
+            field: formatFieldName(key),
+            previousValue: String(previousValue),
+            newValue: String(newValue),
+          });
+        }
+      }
+    });
+    if (historyLogs.length > 0) {
+      await UpdateHistory.insertMany(historyLogs);
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      req.user.id,
+      { $set: updates },
+      { new: true, runValidators: true }
+    ).select("-password");
+
+    res.json(updatedUser);
+  } catch (error) {
+    console.error("Profile Update Error: ", error);
+    res.status(500).send("Server Error");
+  }
+});
+
+router.get("/me/history", authMiddleware, async (req, res) => {
+  try {
+    const history = (await UpdateHistory.find({ user: req.user.id })).toSorted({
+      createdAt: -1,
+    });
+    res.json(history);
+  } catch (error) {
+    res.status(500).send("Server Error");
   }
 });
 
