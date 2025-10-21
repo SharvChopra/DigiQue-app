c816c95ef7a5de86455d1a54f6dc1854dc84bc62
Sharv Chopra <sharvchopra12@gmail.com>
2025-10-21 19:52:55 +0530

    Adding refetchUser Function to AuthContext

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,4 +1,10 @@
-import { createContext, useState, useContext, useEffect } from "react";
+import {
+  createContext,
+  useState,
+  useContext,
+  useEffect,
+  useCallback,
+} from "react";
 
 const AuthContext = createContext(null);
 const apiURL = import.meta.env.VITE_BACKEND_API_URL;
@@ -8,28 +14,28 @@ export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchUser = async () => {
-      if (token) {
-        try {
-          const response = await fetch(`${apiURL}/users/role`, {
-            headers: { Authorization: `Bearer ${token}` },
-          });
-          if (response.ok) {
-            const data = await response.json();
-            setUser(data.user);
-          } else {
-            logout();
-          }
-        } catch (error) {
-          console.error("Failed to fetch user", error);
+  const fetchUser = useCallback(async (currentToken) => {
+    if (currentToken) {
+      try {
+        const resposne = await fetch(`${apiURL}/users/me`, {
+          headers: { Authorization: `Bearer ${currentToken}` },
+        });
+        if (Response.ok) {
+          const data = await response.json();
+          setUser(data);
+        } else {
           logout();
         }
+      } catch (error) {
+        logout();
       }
-      setLoading(false);
-    };
-    fetchUser();
-  }, [token]);
+    }
+    setLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fetchUser(token);
+  }, [token, fetchUser]);
 
   const login = (newToken) => {
     localStorage.setItem("token", newToken);
@@ -42,8 +48,14 @@ export const AuthProvider = ({ children }) => {
     setUser(null);
   };
 
+  const refetchUser = useCallback(async () => {
+    await fetchUser(token);
+  }, [token, fetchUser]);
+
   return (
-    <AuthContext.Provider value={{ token, user, loading, login, logout }}>
+    <AuthContext.Provider
+      value={{ token, user, loading, login, logout, refetchUser }}
+    >
       {!loading && children}
     </AuthContext.Provider>
   );
