bd169dabc3ea61510f5e35bd901bd27173acebc7
Sharv Chopra <sharvchopra12@gmail.com>
2025-10-21 20:33:20 +0530

    Updated Protected Route

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -2,9 +2,11 @@ import { Navigate, useLocation } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
 const ProtectedRoute = ({ children }) => {
-  const { token } = useAuth();
+  const { token, loading } = useAuth();
   const location = useLocation();
-
+  if (loading) {
+    return <div>Loading...</div>;
+  }
   if (!token) {
     return <Navigate to="/sign-in" state={{ from: location }} replace />;
   }
